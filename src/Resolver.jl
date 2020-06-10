65eb15bdab164ed37581dba8a88faba25546724e
Stefan Karpinski <stefan@karpinski.org>
2020-06-10 14:39:16 -0400

    wip

--- src/Resolver.jl
+++ src/Resolver.jl
@@ -1,35 +1,74 @@
 module Resolver
 
-function parse_pkg(pkg::AbstractString)
+const Node = Tuple{String,Int}
+
+function parse_pkg(pkg::AbstractString)::String
     occursin(r"^[a-z]+$"i, pkg) ? pkg : error("invalid package name: $pkg")
 end
 
-function parse_deps(deps::AbstractString)
-    map(parse_pkg, split(strip(deps), r"\s*,\s*", keepempty=false))
-end
-
-function parse_ver(ver::AbstractString)
+function parse_ver(ver::AbstractString)::Node
     m = match(r"^([a-z]+)([1-9][0-9]*)$"i, ver)
     m === nothing && error("invalid package version: $ver")
     String(m.captures[1]), parse(Int, m.captures[2])
 end
 
-function graph(
-    deps::Dict{String,String},
-    conflicts::Vector{Tuple{String,String}},
+function parse_list(f::Function, list::AbstractString)
+    map(f, split(strip(list), r"\s*,\s*", keepempty=false))
+end
+parse_pkgs(list::AbstractString) = parse_list(parse_pkg, list)
+parse_vers(list::AbstractString) = parse_list(parse_ver, list)
+
+function add_node!(nodes::Vector{Node}, node::Node)
+    node in nodes && return
+    not = (node[1], 0)
+    not in nodes || push!(nodes, not)
+    push!(nodes, node)
+    return
+end
+
+function add_edge!(
+    nodes::Vector{Node},
+    edges::Vector{Tuple{Node,Node}},
+    edge::Tuple{Node,Node},
 )
-    nodes = sort!(unique!(map(parse_ver, collect(keys(deps)))))
-    edges = Tuple{Tuple{String,Int},Tuple{String,Int}}[
-        (parse_ver(a), parse_ver(b)) for (a, b) in conflicts
-    ]
-    for (v, d) in deps
-        ver = parse_ver(v)
-        for dep in parse_deps(d)
-            push!(edges, (ver, (dep, 0)))
+    edge in edges && return
+    add_node!(nodes, edge[1])
+    add_node!(nodes, edge[2])
+    push!(edges, edge)
+    push!(edges, reverse(edge))
+    return
+end
+
+function add_edge!(
+    nodes::Vector{Node},
+    edges::Vector{Tuple{Node,Node}},
+    edge::Pair{Node,String},
+)
+    add_edge!(nodes, edges, (edge[1], (edge[2], 0)))
+end
+
+function graph(
+    dependencies::Dict{String,String},
+    conflicts::Vector{Tuple{String,String}} = Tuple{String,String}[],
+)
+    # construct the graph
+    nodes = Node[]
+    edges = Tuple{Node,Node}[]
+    for (vers, deps) in dependencies
+        for ver in parse_vers(vers),
+            pkg in parse_pkgs(deps)
+            add_edge!(nodes, edges, ver => pkg)
         end
     end
-    sort!(union!(edges, map(reverse, edges)))
-    sort!(union!(nodes, map(first, edges)))
+    for (vers1, vers2) in conflicts
+        for v1 in parse_vers(vers1),
+            v2 in parse_vers(vers2)
+            add_edge!(nodes, edges, (v1, v2))
+        end
+    end
+    sort!(nodes)
+    sort!(edges)
+
     # construct the neighbor lists
     n = length(nodes)
     N = [[i] for i = 1:n]

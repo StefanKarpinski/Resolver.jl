8d8960f492e5dd67c6b1d3903ef34c01f1dd3369
Stefan Karpinski <stefan@karpinski.org>
2021-11-30 21:05:47 -0500

    brute force: return lexigraphically optimal solutions
    This fixes the tests by changing them: previously we were generating all
    Pareto-optimal solutions, i.e. any solution that is not dominated by any
    other solution. It turns out that's not what the algorithm I've written
    computes. What it computes is the solutions that are lexicographically
    optimal under some permutation of packages.

--- test/setup.jl
+++ test/setup.jl
@@ -1,6 +1,7 @@
-using Test
+using Combinatorics
 using Random
 using Resolver
+using Test
 
 function resolve_brute_force(
     packages  :: AbstractVector{<:AbstractVector{<:Integer}},
@@ -8,37 +9,33 @@ function resolve_brute_force(
 )
     N = length(packages)
     M = sum(length, packages)
-    Π = prod(length, packages)
-    Π > 0 && log2(Π) ≈ sum(log2∘length, packages) ||
+    P = prod(length, packages)
+    P > 0 && log2(P) ≈ sum(log2∘length, packages) ||
         throw(ArgumentError("brute force only works for small problems"))
 
     # generate all conflict-free solutions
     solution = zeros(Int, N)
-    solutions = Vector{Int}[]
-    for S = 0:Π-1
-        b = 1
-        for i = 1:N
-            V = length(packages[i])
-            S, r = divrem(S, V)
-            solution[i] = b + r
-            b += V
-        end
-        any(v₁ ∈ solution && v₂ ∈ solution for (v₁, v₂) in conflicts) && continue
-        push!(solutions, copy(solution))
-    end
-
-    # filter out sub-optimal solutions
-    filter!(solutions) do s₁
-        all(solutions) do s₂
-            equal = true
-            for (v₁, v₂) in zip(s₁, s₂)
-                v₁ < v₂ && return true
-                equal &= (v₁ == v₂)
+    solutions = typeof(solution)[]
+    for π in permutations(1:N)
+        for S = 0:P-1
+            for i = 1:N
+                V = length(packages[π[i]])
+                S, r = divrem(S, V)
+                solution[π[i]] = r
             end
-            return equal
+            b = 1
+            for i = 1:N
+                solution[i] += b
+                b += length(packages[i])
+            end
+            if !any(v₁ ∈ solution && v₂ ∈ solution for (v₁, v₂) in conflicts)
+                solution ∉ solutions && push!(solutions, copy(solution))
+                break
+            end
         end
+        isempty(solutions) && break
     end
 
     # return sorted vector of sorted solutions
-    return sort!(map(sort!, solutions))
+    return sort!(solutions)
 end

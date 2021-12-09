d1c1277edc75661bd94d4a2dfee20d58e27eaf79
Stefan Karpinski <stefan@karpinski.org>
2021-12-09 17:17:25 -0500

    wip

--- src/Resolver.jl
+++ src/Resolver.jl
@@ -8,26 +8,20 @@ dd(A::AbstractArray) = map(reverse∘bitstring, permutedims(A))
 function resolve(
     packages  :: AbstractVector{<:AbstractVector{<:Integer}},
     conflicts :: AbstractVector{<:Tuple{Integer,Integer}};
-    Block     :: Type{<:Base.BitUnsigned} = UInt,
     sort      :: Bool = true,
 )
-    # vector of solution vectors
-    solutions = Vector{Int}[]
-
     # counts & sizes
-    N = length(packages)                    # number of packages
-    M = mapreduce(maximum, max, packages)   # number of versions
-    d = 8*sizeof(Block)                     # size of a version block
-    m = div(M, d, RoundUp)                  # number of version blocks
+    M = length(packages)                    # number of packages
+    N = mapreduce(maximum, max, packages)   # number of versions
 
     # check packages
-    let counts = zeros(Int, M)
+    let counts = zeros(Int, N)
         for (p, versions) in enumerate(packages), v in versions
-            1 ≤ v ≤ M ||
+            1 ≤ v ≤ N ||
                 throw(ArgumentError("invalid version index: $v"))
             counts[v] += 1
         end
-        for v = 1:M
+        for v = 1:N
             counts[v] == 1 ||
                 throw(ArgumentError("version $v in $(counts[v]) packages"))
         end
@@ -35,118 +29,53 @@ function resolve(
 
     # check conflicts
     for (v1, v2) in conflicts
-        1 ≤ v1 ≤ M  || throw(ArgumentError("invalid version index: $v1"))
-        1 ≤ v2 ≤ M  || throw(ArgumentError("invalid version index: $v2"))
+        1 ≤ v1 ≤ N  || throw(ArgumentError("invalid version index: $v1"))
+        1 ≤ v2 ≤ N  || throw(ArgumentError("invalid version index: $v2"))
     end
 
+    # no packages, empty solution
+    M == 0 && return push!(solutions, Int[])
     # no versions, no solutions
-    M == 0 && return solution
+    N == 0 && return solution
 
-    # Block unit
-    𝟙 = one(Block)
-
-    # construct blocked preference matrix
-    P = zeros(Block, m, M)
-        # each column is for a version
-        # each row is a block of preference bitmask
-
-    # earlier versions of the same package are preferred
-    for (p1, versions1) in enumerate(packages),
-        (p2, versions2) in enumerate(packages)
-        for v1 in versions1, v2 in versions2
-            p1 == p2 && v1 <= v2 && continue
-            b, s = divrem(v2-1, d)
-            P[b+1, v1] |= 𝟙 << s
-        end
+    # adjacency lists: incompatible & compatible
+    X = [Int[] for v = 1:N]
+    C = [Int[] for v = 1:N]
+    for (p1, V1) in enumerate(packages), v1 in V1,
+        (p2, V2) in enumerate(packages), v2 in V2
+        x = p1 == p2 || (v1, v2) ∈ conflicts || (v2, v2) ∈ conflicts
+        push!((x ? X : C)[v1], v2)
     end
 
-    # construct blocked conflicts matrix
-    X = zeros(Block, m, M)
-        # each column is for a version
-        # each row is a block of conflict bitmask
+    # level vector
+    L = zeros(Int, N)
 
-    # explicit conflicts are incompatible, of course
-    for (v1, v2) in conflicts
-        # conflicts are symmetrized
-        b1, s1 = divrem(v1-1, d)
-        b2, s2 = divrem(v2-1, d)
-        X[b1+1, v2] |= 𝟙 << s1
-        X[b2+1, v1] |= 𝟙 << s2
-    end
+    # solution vector
+    S = zeros(Int, M)
 
-    # different versions of the same package are incompatible too
-    for (p, versions) in enumerate(packages)
-        for v1 in versions, v2 in versions
-            b, s = divrem(v2-1, d)
-            X[b+1, v1] |= 𝟙 << s
-        end
-    end
+    # vector of solution vectors
+    solutions = typeof(S)[]
 
-    # allocate iteration candidates matrix
-    C = zeros(Block, m, N)
-        # each column is for a recursion level
-        # each row is a block of candidate bitmask
-    # turn all candidates on in first column
-    for i = 1:m-1
-        C[i, 1] = -𝟙
-    end
-    # except the extra bits in the last block
-    let s = mod(-M, d)
-        C[m, 1] = -𝟙 << s >> s
-    end
-
-    # allocate recursion candidates matrix
-    R = zeros(Block, m, N-1)
-        # each column is for a recursion level
-        # each row is a block of candidate bitmask
-
-    # allocate selections vector
-    S = zeros(Int, N)
-
-    function search!(r::Int=1)
-        # copy recursion candidates from iteration candidates
-        if r < N
-            for i = 1:m
-                R[i, r] = C[i, r]
-            end
-        end
-        # initial block & shift values
-        b = s = 0
-        while true
-            # look for the next candidate
-            let c = C[b+1, r]
-                if c >> s == 0 # no more candidates in current block
-                    b += 1 # next block
-                    while b < m
-                        c = C[b+1, r] # candidates bitmask
-                        c != 0 && break # there's one in this block
-                        b += 1
-                    end
+    function search!(r::Int = 0)
+        @show r, L, S[1:r]
+        # consider pivot and its incompatibilities
+        for i = 1:N
+            l = L[i]
+            if l == r
+                S[r+1] = i
+                if r+1 == M
+                    push!(solutions, copy(S))
+                    continue
                 end
-                # shift for next candidate
-                s += trailing_zeros(c >> s)
-            end
-            v = b*d + s + 1
-            v ≤ M || break
-            # record candidate version
-            S[r] = v
-            # recurse or save solution
-            if r < N
-                # only compatible versions lower down
-                for i = 1:m
-                    C[i, r+1] = R[i, r] & ~X[i, v]
+                for j in C[i]
+                    L[j] += (L[j] == r)
                 end
-                search!(r+1)
-            else
-                # for each version in this solution, only try strictly
-                # better versions of the same package at higher levels
-                for r′ = 1:N, r′′ = 1:r′, i = 1:m
-                    C[i, r′′] &= P[i, S[r′]]
+                search!(r + 1)
+                for j in C[i]
+                    L[j] = max(L[j], r)
                 end
-                push!(solutions, copy(S))
-                break
+                L[i] = r-1
             end
-            s += 1
         end
     end
     search!()

130e651d3640e116b50a238220920651d56e5190
Stefan Karpinski <stefan@karpinski.org>
2022-08-10 12:43:59 -0400

    test/registry.jl: update for new API
    Yay, this is now fast enough to prepare DifferentialEquations!

--- test/registry.jl
+++ test/registry.jl
@@ -1,49 +1,41 @@
 using Pkg: depots1
-using Pkg.Registry: PkgInfo, RegistryInstance, init_package_info!
+using Pkg.Registry: RegistryInstance, init_package_info!
 using Pkg.Types: stdlibs
 using Pkg.Versions: VersionSpec
 
-const EXCLUDED_DEPS = push!(Set(values(stdlibs())), "julia")
+const reg_path = joinpath(depots1(), "registries", "General.toml")
+const reg_inst = RegistryInstance(expanduser(reg_path))
+const reg_dict = Dict(p.name => p for p in values(reg_inst.pkgs))
+const excludes = push!(Set(first.(values(stdlibs()))), "julia")
 
-const reg_path = "~/.julia/registries/General.toml"
-const reg = RegistryInstance(expanduser(reg_path))
-
-const PKG_INFO = Dict{String,PkgInfo}()
-
-pkg_info(p::AbstractString) = get!(PKG_INFO, p) do
-    for pkg in values(reg.pkgs)
-        pkg.name == p && return init_package_info!(pkg)
-    end
-    error("package $p not found")
-end
-
-function versions_callback(p::AbstractString)
-    info = pkg_info(p)
-    sort!(collect(keys(info.version_info)), rev=true)
-end
-
-function deps_callback(p::AbstractString, v::VersionNumber)
-    info = pkg_info(p)
-    deps = Set{String}()
-    for (r, d) in info.deps
-        v in r && union!(deps, keys(d))
-    end
-    return setdiff(deps, EXCLUDED_DEPS)
-end
-
-function compat_callback(
-    p::AbstractString, v::VersionNumber;
-    versions::Function = versions_callback,
-)
-    info = pkg_info(p₁)
-    compat = Dict{String,Vector{VersionNumber}}()
-    for (rₚ, c) in info.compat
-        v in r || continue
-        for (d, rᵈ) in c
-            vers = get!(compat, d) do
-                versions(d)
-            end
-            filter!(in(rᵈ), vers)
+reg_deps = DepsProvider{String, VersionNumber, VersionSpec}() do pkg::String
+    info = init_package_info!(reg_dict[pkg])
+    vers = sort!(collect(keys(info.version_info)), rev=true)
+    deps = Dict(v => String[] for v in vers)
+    comp = Dict(v => Dict{String,VersionSpec}() for v in vers)
+    # scan versions and populate deps & compat data
+    for v in vers
+        for (r, d) in info.deps
+            v in r && union!(deps[v], keys(d))
+        end
+        for (r, c) in info.compat
+            v in r && mergewith!(intersect, comp[v], c)
         end
     end
+    foreach(sort!, values(deps))
+    # scrub out excluded deps (stdlibs, julia itself)
+    for d in values(deps)
+        setdiff!(d, excludes)
+    end
+    for c in values(comp), x in excludes
+        delete!(c, x)
+    end
+    # deduplicate data structures to save memory
+    for i = 1:length(vers)-1, j = i+1:length(vers)
+        v, w = vers[i], vers[j]
+        deps[v] == deps[w] && (deps[v] = deps[w])
+        comp[v] == comp[w] && (comp[v] = comp[w])
+    end
+    # return resolver PkgInfo data structure
+    PkgInfo{String, VersionNumber, VersionSpec}(vers, deps, comp)
 end

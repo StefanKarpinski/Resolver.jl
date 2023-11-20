9a752dc3328f4b6e59e643dbe6577354e6340e3f
Stefan Karpinski <stefan@karpinski.org>
2023-11-20 12:25:41 -0500

    better optimality with conflict indicator variables

--- src/Solve.jl
+++ src/Solve.jl
@@ -17,23 +17,36 @@ picosat_sat(p::Ptr{Cvoid}, limit::Integer = -1) =
 picosat_deref(p::Ptr{Cvoid}, lit::Integer) =
     ccall((:picosat_deref, libpicosat), Cint, (Ptr{Cvoid}, Cint), p, lit)
 
+function picosat_print(p::Ptr{Cvoid}, path::AbstractString)
+    f = ccall(:fopen, Ptr{Cvoid}, (Cstring, Cstring), path, "w")
+    ccall((:picosat_print, libpicosat), Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}), p, f)
+    @assert ccall(:fclose, Cint, (Ptr{Cvoid},), f) == 0
+end
+
 function resolve(
     info :: Dict{P, PkgInfo{P,V}},
     reqs :: SetOrVec{P},
 ) where {P,V}
     # sort stuff for determinism
-    names = sort!(collect(keys(info)))
     reqs = sort!(collect(reqs))
+    names = sort!(collect(keys(info)))
+    sort!(names, by = !in(reqs))
 
     # variable indices:
-    #   var[p]      package p chosen
-    #   var[p]+i    version p@i chosen
-    N = 0
+    #   p    var[p]        package p chosen
+    #   p@i  var[p]+i      version i of p chosen
+    #   p!i  var[p]+n_p+i  version i of p conflicted
+    N = 1
     var = Dict{P, Int}()
     for p in names
-        var[p] = N += 1
-        N += length(info[p].versions)
+        var[p] = N
+        n_p = length(info[p].versions)
+        # 1 varible for package
+        # n_p variables for versions
+        # n_p variables for conflicts
+        N += 2n_p + 1
     end
+    N -= 1 # last used variable
 
     # instantiate picosat solver
     ps = picosat_init()
@@ -47,6 +60,7 @@ function resolve(
         info_p = info[p]
         v_p = var[p]
         n_p = length(info_p.versions)
+        x_p = v_p + n_p
 
         # package implies some version
         #   p => OR_i p@i
@@ -85,42 +99,50 @@ function resolve(
             end
         end
 
-        # conflicts
-        #   !p@i OR !q@j
-        for q in sort!(collect(keys(info_p.interacts)))
-            # symmetric, only emit once
-            q ≥ p && break
-            b = info_p.interacts[q]
-            v_q = var[q]
-            n_q = length(info[q].versions)
-            for i = 1:n_p, j = 1:n_q
-                info_p.conflicts[i, b+j] || continue
-                picosat_add(ps, -(v_p + i))
-                picosat_add(ps, -(v_q + j))
+        # conflict indicators
+        interacts = sort!(collect(keys(info_p.interacts)))
+        for i = 1:n_p
+            # collect this version's conflicts
+            conflicts = Vector{Pair{P,Int}}()
+            for q in interacts
+                b = info_p.interacts[q]
+                n_q = length(info[q].versions)
+                for j = 1:n_q
+                    info_p.conflicts[i, b+j] || continue
+                    push!(conflicts, q => j)
+                end
+            end
+
+            # conflict indicator implies some conflicting version
+            #   p!i => conflicts(p, i)
+            picosat_add(ps, -(x_p + i))
+            for (q, j) in conflicts
+                picosat_add(ps, var[q] + j)
+            end
+            picosat_add(ps, 0)
+
+            # each conflicting version implies conflict indicator
+            #   ∀ q@j ∈ conflicts(p, i): q@j => p!i
+            for (q, j) in conflicts
+                picosat_add(ps, -(var[q] + j))
+                picosat_add(ps, x_p + i)
                 picosat_add(ps, 0)
             end
         end
 
-        # optimality -- one of these has to hold:
-        #   1. package isn't chosen
-        #   2. version or better chosen
-        #   3. conflicting version chosen
-        # that is:
-        #   !p OR p@1 ... p@i OR conflicts(p, i)
+        # conflict indicator blocks actual version
+        #   p!i => !p@i
         for i = 1:n_p
-            picosat_add(ps, -v_p)
-            for j = 1:i
-                picosat_add(ps, v_p + j)
-            end
-            for q in sort!(collect(keys(info_p.interacts)))
-                b = info_p.interacts[q]
-                info_q = info[q]
-                v_q = var[q]
-                for j = 1:length(info_q.versions)
-                    info_p.conflicts[i, b+j] || continue
-                    picosat_add(ps, v_q + j)
-                end
-            end
+            picosat_add(ps, -(x_p + i))
+            picosat_add(ps, -(v_p + i))
+            picosat_add(ps, 0)
+        end
+
+        # optimality -- version => all better have conflicts
+        #   ∀ j < i: p@i => p#i
+        for i = 1:n_p, j = 1:i-1
+            picosat_add(ps, -(v_p + i))
+            picosat_add(ps, x_p + j)
             picosat_add(ps, 0)
         end
     end
@@ -180,10 +202,14 @@ function resolve(
     end
 
     # sort solutions lexicographically
-    pkgs = sort!(collect(mapreduce(keys, union, sols)))
-    sort!(pkgs, by = !in(reqs)) # required ones first
-    for p in reverse(pkgs)
-        sort!(sols, by = sol -> get(sol, p, v"0-"), rev=true)
+    if !isempty(sols)
+        pkgs = sort!(collect(mapreduce(keys, union, sols)))
+        sort!(pkgs, by = !in(reqs)) # required ones first
+        for p in reverse(pkgs)
+            sort!(sols, by = sol -> get(sol, p, v"0-"), rev=true)
+        end
+    else
+        pkgs = Vector{P}()
     end
 
     # versions as a matrix

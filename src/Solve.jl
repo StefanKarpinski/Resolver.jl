2987ab19332dbc844b299fd55e73a282b01974de
Stefan Karpinski <stefan@karpinski.org>
2023-11-21 16:00:24 -0500

    simpler approach: find any valid solution, then optimize it
    The idea is that the core problem specification doesn't try to include
    any optimality or minimality--it just encodes what it means to be a
    complete, conflict-free set of versions package requirements. To find an
    optimal solutions, we find any solution and then optimize it by
    improving it until it cannot be improved any more. We find the next
    optimal solution by finding any solution that improves on that solution
    in some aspect and then optimize that and so on.

--- src/Solve.jl
+++ src/Solve.jl
@@ -37,9 +37,8 @@ function resolve(
     sort!(names, by = !in(reqs))
 
     # variable indices:
-    #   p    var[p]        package p chosen
-    #   p@i  var[p]+i      version i of p chosen
-    #   p!i  var[p]+n_p+i  version i of p conflicted
+    #   p    var[p]     package p chosen
+    #   p@i  var[p]+i   version i of p chosen
     N = 1
     var = Dict{P, Int}()
     for p in names
@@ -47,8 +46,7 @@ function resolve(
         n_p = length(info[p].versions)
         # 1 varible for package
         # n_p variables for versions
-        # n_p variables for conflicts
-        N += 2n_p + 1
+        N += 1 + n_p
     end
     N -= 1 # last used variable
 
@@ -57,15 +55,11 @@ function resolve(
     sols = try
         picosat_adjust(ps, N)
 
-        # track reverse dependencies
-        rdeps = Dict{P, Vector{Pair{P,Int}}}()
-
         # generate SAT problem
         for p in names
             info_p = info[p]
-            v_p = var[p]
             n_p = length(info_p.versions)
-            x_p = v_p + n_p
+            v_p = var[p]
 
             # package implies some version
             #   p => OR_i p@i
@@ -85,7 +79,7 @@ function resolve(
 
             # versions are mutually exclusive
             #   !p@i OR !p@j
-            for i = 1:n_p, j = 1:i-1
+            for i = 1:n_p-1, j = i+1:n_p
                 picosat_add(ps, -(v_p + i))
                 picosat_add(ps, -(v_p + j))
                 picosat_add(ps, 0)
@@ -99,92 +93,39 @@ function resolve(
                     picosat_add(ps, -(v_p + i))
                     picosat_add(ps, var[q])
                     picosat_add(ps, 0)
-                    # save reverse dependency
-                    push!(get!(() -> Pair{P,Int}[], rdeps, q), p => i)
                 end
             end
 
-            # conflict indicators
-            interacts = sort!(collect(keys(info_p.interacts)))
+            # conflicts
+            #   ∀ q@j ∈ conflicts(p, i): !p@i OR !q@j
             for i = 1:n_p
-                # collect this version's conflicts
-                conflicts = Vector{Pair{P,Int}}()
-                for q in interacts
-                    b = info_p.interacts[q]
+                for (q, b) in info_p.interacts
+                    p < q || continue # conflicts are symmetrical
                     n_q = length(info[q].versions)
+                    v_q = var[q]
                     for j = 1:n_q
                         info_p.conflicts[i, b+j] || continue
-                        push!(conflicts, q => j)
+                        # conflicting versions are mutually exclusive
+                        picosat_add(ps, -(v_p + i))
+                        picosat_add(ps, -(v_q + j))
+                        picosat_add(ps, 0)
                     end
                 end
-
-                # conflict indicator implies some conflicting version
-                #   p!i => conflicts(p, i)
-                picosat_add(ps, -(x_p + i))
-                for (q, j) in conflicts
-                    picosat_add(ps, var[q] + j)
-                end
-                picosat_add(ps, 0)
-
-                # each conflicting version implies conflict indicator
-                #   ∀ q@j ∈ conflicts(p, i): q@j => p!i
-                for (q, j) in conflicts
-                    picosat_add(ps, -(var[q] + j))
-                    picosat_add(ps, x_p + i)
-                    picosat_add(ps, 0)
-                end
             end
-
-            # conflict indicator blocks actual version
-            #   p!i => !p@i
-            for i = 1:n_p
-                picosat_add(ps, -(x_p + i))
-                picosat_add(ps, -(v_p + i))
-                picosat_add(ps, 0)
-            end
-
-            # optimality (package-wise unilateral improvement)
-            #   ∀ j < i: p@i => p!j
-            #
-            # note: it's still possible to get a suboptimal solution in a case where
-            # p@i′ > p@i and p@j′ > p@j but p@i conflicts p@j′ and p@j conflicts
-            # p@i′ so you have to change both at once in order to actually improve;
-            # this rule can't prevent that but we force actual improvement when
-            # searching and then we filter out fully dominated solutions
-            #
-            for i = 1:n_p, j = 1:i-1
-                picosat_add(ps, -(v_p + i))
-                picosat_add(ps, x_p + j)
-                picosat_add(ps, 0)
-            end
-        end
-
-        # minimality (only packages that something depends on)
-        #   p => revdeps(p)
-        #
-        # note: this can be foiled by dependency loops, e.g. where p@i depends on
-        # q@j and vice versa, but nothing external actually depends on either one;
-        # this rule can't't prevent that, but we filter these out later
-        #
-        for (q, rdeps_q) in sort!(collect(rdeps), by=first)
-            picosat_add(ps, -var[q])
-            for (p, i) in rdeps_q
-                picosat_add(ps, -(var[p] + i))
-            end
-            picosat_add(ps, 0)
         end
 
         # add requirements clauses
-        #   these are the packages that are required
+        #   ∀ p in reqs: p
         for p in reqs
             picosat_add(ps, var[p])
             picosat_add(ps, 0)
         end
 
+        # helper functions for finding optimal solutions
+
         function extract_solution!(sol::Dict{P,Int})
             empty!(sol)
-            for p in names
-                v_p = var[p]
+            for (p, v_p) in var
                 picosat_deref(ps, v_p) < 0 && continue
                 for i = 1:length(info[p].versions)
                     if picosat_deref(ps, v_p + i) > 0
@@ -192,6 +133,23 @@ function resolve(
                         break
                     end
                 end
+            end
+            return sol
+        end
+
+        function improve_solution!(improve::Function, sol::Dict{P,Int})
+            while true
+                # save main problem state (we add temporary clauses)
+                picosat_push(ps)
+                # apply improvement clauses
+                improve()
+                # recheck satisfiability
+                sat′ = picosat_sat(ps)
+                sat′ == PICOSAT_SATISFIABLE && extract_solution!(sol)
+                # pop temporary clauses
+                picosat_pop(ps)
+                # fully optimized if not satisfiable
+                sat′ == PICOSAT_SATISFIABLE || break
             end
             return sol
         end
@@ -205,66 +163,44 @@ function resolve(
             sat == PICOSAT_SATISFIABLE || break
             extract_solution!(sol)
 
-            # minimize solution
-            while true
-                # save main problem state
-                picosat_push(ps)
-
-                # disallow adding packages
-                for p in setdiff(names, keys(sol))
-                    picosat_add(ps, -var[p])
-                    picosat_add(ps, 0)
-                end
-
-                # disallow changing versions
-                for (p, i) in sol
+            # optimize solution (allow adding packages)
+            improve_solution!(sol) do
+                # disallow non-improvements to each package
+                for (p, j) in sol
                     v_p = var[p]
-                    picosat_add(ps, v_p + i)
+                    # allow as-good-or-better versions
+                    for i = 1:j
+                        picosat_add(ps, v_p + i)
+                    end
+                    # allow dropping package entirely
                     picosat_add(ps, -v_p)
                     picosat_add(ps, 0)
                 end
-
-                # require dropping some packages
-                for (p, i) in sol
-                    picosat_add(ps, -var[p])
+                # require a some improvement
+                for (p, j) in sol
+                    v_p = var[p]
+                    for i = 1:j-1
+                        picosat_add(ps, v_p + i)
+                    end
+                    picosat_add(ps, -v_p)
                 end
                 picosat_add(ps, 0)
-
-                # recheck satisfiability
-                sat′ = picosat_sat(ps)
-                sat′ == PICOSAT_SATISFIABLE && extract_solution!(sol)
-
-                # pop temporary clauses
-                picosat_pop(ps)
-
-                # fully minimized
-                sat′ == PICOSAT_SATISFIABLE || break
             end
 
-            # drop earlier solutions that are strictly worse
-            filter!(sols) do sol′
-                for (p, i) in sol
-                    j = get(sol′, p, i)
-                    j < i && return true # keep
-                end
-                return false # reject
-            end
-
-            # save minimized solution
+            # save optimized solution
             push!(sols, copy(sol))
 
-            # next solution has to improve somehow
-            for (p, i) in sol
+            # next solution must improve some package
+            for (p, j) in sol
                 v_p = var[p]
-                for j = 1:i-1
-                    picosat_add(ps, var[p] + j)
+                for i = 1:j-1
+                    picosat_add(ps, v_p + i)
                 end
             end
             picosat_add(ps, 0)
         end
 
         sols # value of the try block
-
     finally
         # destroy picosat solver
         picosat_reset(ps)
@@ -279,9 +215,9 @@ function resolve(
     end
 
     # sort solutions
-    for p in reverse(pkgs)
-        sort!(sols, by = sol -> get(sol, p, 0))
-    end
+    # for p in reverse(pkgs)
+    #     sort!(sols, by = sol -> get(sol, p, 0))
+    # end
 
     # versions as a matrix
     vers = Union{V, Nothing}[
@@ -296,11 +232,12 @@ end
 #=
 using PrettyTables
 pkgs, vers = resolve(info, reqs)
-max_vers = vec(mapslices(r->maximum(v->something(v, v"0-"), r), vers, dims=2))
-pretty_table([pkgs vers],
-    highlighters = Highlighter(
-        (data, i, j) -> j > 1 && something(data[i, j], max_vers[i]) < max_vers[i],
-        foreground = :red,
+let max_vers = vec(mapslices(r->maximum(v->something(v, v"0-"), r), vers, dims=2))
+    pretty_table([pkgs vers],
+        highlighters = Highlighter(
+            (data, i, j) -> j > 1 && something(data[i, j], max_vers[i]) < max_vers[i],
+            foreground = :red,
+        )
     )
-)
+end
 =#

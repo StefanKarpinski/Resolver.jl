4c38e5fa742719bf1c7f459fc5c8b080fa05e4ee
Stefan Karpinski <stefan@karpinski.org>
2023-12-08 11:10:13 -0500

    test/setup refactor: extract valid check, partial order gen

--- test/setup.jl
+++ test/setup.jl
@@ -26,71 +26,114 @@ function test_resolve(
     @test M == length(pkgs)
     @test all(haskey(data, p) for p in pkgs)
     @test all(
-        isnothing(vers[i, s]) ||
-        vers[i, s] ∈ data[p].versions
+        isnothing(vers[i, k]) ||
+        vers[i, k] ∈ data[p].versions
         for (i, p) in enumerate(pkgs)
-        for s = 1:N
+        for k = 1:N
     )
 
-    # checking validity of a solution
-    function is_valid_solution(s::AbstractVector{Union{V,Nothing}})
-        # check satisfiaction of dependencies
-        for (i, v) in enumerate(s)
-            v === nothing && continue
-            data_p = data[pkgs[i]]
-            haskey(data_p.depends, v) || continue
-            for q in data_p.depends[v]
-                q ∈ pkgs || return false
-                j = findfirst(==(q), pkgs)
-                isnothing(s[j]) && return false
-            end
-        end
-        # check compatibility of versions
-        for i = 1:M, j = 1:M
-            v = s[i]; isnothing(v) && continue
-            w = s[j]; isnothing(w) && continue
-            p = pkgs[i]
-            q = pkgs[j]
-            haskey(data[p].compat, v) &&
-            haskey(data[p].compat[v], q) &&
-            w ∉ data[p].compat[v][q] && return false
-            haskey(data[q].compat, w) &&
-            haskey(data[q].compat[w], p) &&
-            v ∉ data[q].compat[w][p] && return false
-        end
-        return true
-    end
-
-    # check each returned solution
+    # check validity of returned solutions
     for k = 1:N
-        s = @view vers[:, k]
-        @test is_valid_solution(s)
+        @test is_valid_solution(data, pkgs, @view vers[:, k])
     end
 
-    # verion dependencies (may replace & expand pkgs)
-    deps = Dict{Tuple{V,Int},Vector{Int}}()
+    # check that solutions can't be trivially improved
+    for k = 1:N
+        s = vers[:, k]
+        for i = 1:M
+            v = vers[i, k]
+            vers_p = data[pkgs[i]].versions
+            r = something(findfirst(==(v), vers_p), 0)
+            for (r′, v′) in enumerate(vers_p)
+                r′ < r || break
+                s[i] = v′
+                @test !is_valid_solution(data, pkgs, s)
+            end
+            s[i] = v # return to original value
+        end
+    end
+
+    # solution partial order
+    ≤ₛ = make_solution_partial_order!(data, reqs, pkgs)
+    # After this:
+    # - pkgs contains all packages that could be needed by any
+    #   version of any package in the original pkgs
+    # - this allows any possible solution to be expressed, not just
+    #   the solutions originally presented
+
+    # check that no solution is dominated by any other
+    for k = 1:N, l = 1:N
+        k ≠ l || continue
+        s = @view vers[:, k]
+        t = @view vers[:, l]
+        @test !(s ≤ₛ t)
+    end
+end
+
+# checking validity of a solution
+function is_valid_solution(
+    data :: AbstractDict{P,<:PkgData{P,V}},
+    pkgs :: AbstractVector{P},
+    vers :: AbstractVector{Union{V,Nothing}};
+) where {P,V}
+    # check satisfiaction of dependencies
+    for (i, v) in enumerate(vers)
+        v === nothing && continue
+        data_p = data[pkgs[i]]
+        haskey(data_p.depends, v) || continue
+        for q in data_p.depends[v]
+            j = something(findfirst(==(q), pkgs), 0)
+            w = get(vers, j, nothing)
+            isnothing(w) && return false
+        end
+    end
+    # check compatibility of versions
+    M = size(vers, 1)
+    for i = 1:M, j = 1:M
+        v = vers[i]; isnothing(v) && continue
+        w = vers[j]; isnothing(w) && continue
+        p = pkgs[i]
+        q = pkgs[j]
+        haskey(data[p].compat, v) &&
+        haskey(data[p].compat[v], q) &&
+        w ∉ data[p].compat[v][q] && return false
+        haskey(data[q].compat, w) &&
+        haskey(data[q].compat[w], p) &&
+        v ∉ data[q].compat[w][p] && return false
+    end
+    return true
+end
+
+# generate ≤ₛ partial order function on solutions
+function make_solution_partial_order!(
+    data :: AbstractDict{P,<:PkgData{P,V}},
+    reqs :: AbstractVector{P},
+    pkgs :: AbstractVector{P}, # may be expanded
+) where {P,V}
+    # verion dependencies (may expand pkgs)
+    deps = Dict{Tuple{Int,V},Vector{Int}}()
     i = 0
     while (i += 1) ≤ length(pkgs)
         p = pkgs[i]
         for v in data[p].versions
-            deps[v,i] = Int[]
+            deps[i,v] = Int[]
             haskey(data[p].depends, v) || continue
             for q in data[p].depends[v]
                 j = findfirst(==(q), pkgs)
                 if isnothing(j)
-                    if length(pkgs) == M
-                        pkgs = copy(pkgs)
-                    end
                     push!(pkgs, q)
                     j = length(pkgs)
                 end
-                push!(deps[v,i], j)
+                push!(deps[i,v], j)
             end
         end
     end
-    # pkgs now contains all packages that could be needed by
-    # any version of any package in the original pkgs and deps
-    # has dependencies for all packages and versions of them
+    # After this:
+    # - pkgs contains all packages that could be needed by any
+    #   version of any package in the original pkgs
+    # - deps has dependencies for all packages and versions of them
+    # - this allows any possible solution to be expressed, not just
+    #   the solutions originally presented
 
     # ranking versions (higher = better)
     ranks = Dict{Tuple{V,Int},Int}()
@@ -127,7 +170,7 @@ function test_resolve(
             for i in need
                 v = get(s, i, nothing)
                 @assert v == get(t, i, nothing)
-                union!(need, deps[v,i])
+                union!(need, deps[i,v])
             end
             n == length(need) && break
         end
@@ -140,14 +183,5 @@ function test_resolve(
             sᵢ > tᵢ && return false
         end
         return true
-    end
-    ≥ₛ(s::Int, t::Int) = ≤ₛ(t, s)
-
-    # check that no solution is dominated by any other
-    for k = 1:N, l = 1:N
-        k ≠ l || continue
-        s = @view vers[:, k]
-        t = @view vers[:, l]
-        @test !(s ≤ₛ t)
     end
 end

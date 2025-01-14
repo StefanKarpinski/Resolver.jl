de563bdda3dfb92a50cc85294e3ac8be7d533741
Stefan Karpinski <stefan@karpinski.org>
2025-01-14 12:03:36 -0500

    bin/resolve.jl: argument parsing (wip)

--- bin/resolve.jl
+++ bin/resolve.jl
@@ -1,8 +1,75 @@
 #!/usr/bin/env julia
 
-push!(empty!(LOAD_PATH), @__DIR__)
+const USAGE = """
+usage: $PROGRAM_FILE [options] [<project path>]
+
+  --julia=<version>       Julia version to resolve for (default: $VERSION)
+  --prioritize=<pkgs>     package names/uuids to prioritize
+  --fix[=<pkgs>]          prefer current full version number
+  --fix-minor[=<pkgs>]    prefer current major.minor version
+  --fix-major[=<pkgs>]    prefer current major version
+  --max[=<pkgs>]          maximize major.minor.patch
+  --max-minor[=<pkgs>]    maximize major.minor; minimize patch
+  --max-major[=<pkgs>]    maximize major; minimize minor.patch
+  --min[=<pkgs>]          minimize major.minor.patch
+  --min-minor[=<pkgs>]    minimize major.minor; maximize patch
+  --min-major[=<pkgs>]    minimize major; maximize minor.patch
+
+Wherever <pkgs> appears you can specify a comma separated list of:
+
+  * Package uuids for any packages
+  * Package names from any the [deps], [weakdeps] or [extras] sections
+    of the specified environment's project file
+  * @deps for all packages in the [deps] section of the project file
+"""
+
+function usage()
+    println(stderr, USAGE)
+    exit(0)
+end
+
+function usage(msg)
+    println(stderr, "[ERROR] $msg\n\n$USAGE")
+    exit(1)
+end
+
+expand_project(path::Nothing) = Base.active_project()
+
+function expand_project(path::AbstractString)
+    isdir(path) && for name in Base.project_names
+        file = joinpath(path, name)
+        Base.isfile_casesensitive(file) && return file
+    end
+    Base.isfile_casesensitive(path) && return path
+    usage("Not a project path: $path")
+end
+
+const OPTS = Dict{String,Union{String,Nothing}}()
+const PROJ = let proj = nothing,
+    opt_re = r"""
+        ^--(
+            julia |
+            prioritize |
+            (?:fix|max|min)(?:-(?:minor|major))?
+        )(?:=(.+))?$
+    """x
+    for arg in ARGS
+        if startswith(arg, "-")
+            arg in ("-h", "--help") && usage()
+            m = match(opt_re, arg)
+            isnothing(m) && usage("Invalid option: $arg")
+            OPTS[m[1]] = m[2]
+        else
+            isnothing(proj) || usage("At most one project can be specified.")
+            proj = arg
+        end
+    end
+    expand_project(proj)
+end
 
 ## imports
+
+push!(empty!(LOAD_PATH), @__DIR__)
 
 import Base: UUID
 import HistoricalStdlibVersions
@@ -18,7 +85,7 @@ import Resolver: DepsProvider, PkgData, resolve
 
 ## sorting of versions and packages
 
-function sort_versions(vers::Set{VersionNumber})
+function sort_versions(uuid::UUID, vers::Set{VersionNumber})
     sort!(collect(vers), rev=true)
 end
 
@@ -92,7 +159,7 @@ dp() = DepsProvider(keys(PACKAGES)) do uuid::UUID
         delete!(c, x)
     end
     # sort versions
-    vers = sort_versions(vers)
+    vers = sort_versions(uuid, vers)
     # deduplicate data structures to save some memory
     for i = 1:length(vers)-1, j = i+1:length(vers)
         v, w = vers[i], vers[j]
@@ -105,13 +172,12 @@ end
 
 ## load project & manifest
 
-path = expanduser("~/dev/HTTP/Project.toml")
-env = EnvCache(path)
+env = EnvCache(PROJ)
 proj = env.project
 uuids = merge(proj.deps, proj.weakdeps, proj.extras)
 uuids["julia"] = JULIA_UUID
-reqs = sort!(filter!(∉(EXCLUDES), collect(values(proj.deps))))
-push!(reqs, JULIA_UUID)
+deps = sort!(filter!(∉(EXCLUDES), collect(values(proj.deps))))
+push!(deps, JULIA_UUID)
 
 # populate COMPAT dict for dependency provider
 for (name, comp) in proj.compat
@@ -120,6 +186,6 @@ end
 
 ## do an actual resolve
 
-pkgs, vers = resolve(dp(), reqs; by = sort_packages_by)
+pkgs, vers = resolve(dp(), deps; by = sort_packages_by)
 names = [first(PACKAGES[u]).name for u in pkgs]
 display([names vers])
